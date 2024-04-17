ccf963680a62959ca5ccd933246e1a9fd95274e2
ExpertNugget <deaconjf04@gmail.com>
2024-04-17 22:49:09 +0000

    progress commit

--- src/main.py
+++ src/main.py
@@ -106,3 +106,24 @@ async def add_points_youtube(userId: str, points: str):
     data["points"] = str(dbpoints)
     db.collection("users").document(doc.id).update({"points": data["points"]})
     return data
+
+
+@app.get("/discordConfig/{branch}/discordToken")
+async def get_discord_token(branch: str):
+    query = db.collection("discordTokens").where(
+        filter=FieldFilter("branch", "==", branch)
+    )
+    docs = query.get()
+    for doc in docs:
+        data = doc.to_dict()
+    return data["botToken"]
+
+
+@app.get("/discordConfig/{guildId}/bumpConfig")
+async def get_bump_config(guildId: str):
+    query = db.collection("discordGuilds").where(
+        filter=FieldFilter("guildId", "==", guildId)
+    )
+    print(guildId)
+    print(query.get())
+    return query.get()
